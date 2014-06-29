98efab6e9e777866a039f454ffddcd8433a878d9
Sean Pont <seanpont@gmail.com>
2014-06-29 13:52:09 -0700

    19: I cheated

--- 019.py
+++ 019.py
@@ -19,4 +19,10 @@ How many Sundays fell on the first of the month during the twentieth
 century (1 Jan 1901 to 31 Dec 2000)?
 """
 
+import datetime
 
+
+print len(tuple(filter(lambda x: x == 0,
+                       (datetime.date(year, month, 1).weekday()
+                        for year in xrange(1901, 2001)
+                        for month in xrange(1, 13)))))-1
