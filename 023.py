74c5d23e6e2fe18765aa4dee6ab59eed979919aa
Sean Pont <seanpont@gmail.com>
2014-06-29 18:48:01 -0700

    an abundant sieve

--- 023.py
+++ 023.py
@@ -23,4 +23,32 @@ Find the sum of all the positive integers which cannot be written as the
 sum of two abundant numbers.
 """
 
+from utils import *
+
+limit = 28123
+
+prime_sieve = PrimeSieve(1000)
+
+def is_abundant(n):
+    return sum(proper_divisors(n, prime_sieve)) > n
+
+abundant_sieve = [False] * limit
+
+for n in xrange(limit):
+    if abundant_sieve[n]: continue
+    if is_abundant(n):
+        for j in xrange(n, limit, n):
+            abundant_sieve[j] = True
+
+def has_abundant_adders(n):
+    for i in xrange(1, n):
+        if abundant_sieve[i] and abundant_sieve[n-i]:
+            return True
+
+print sum((n for n in xrange(1, limit) if not has_abundant_adders(n)))
+
+#
+# finder = find_adders(abundants)
+# print 'identifying numbers than cannot be expressed as the sum of two abundant numbers'
+# print sum((i for i in range(limit) if not finder(i)))
 
