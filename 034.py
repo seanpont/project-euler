db903b17032601152919e899a3784bf4f148160e
Sean Pont <seanpont@gmail.com>
2014-07-16 18:51:52 -0700

    34: digits, factorials, 1h

--- 034.py
+++ 034.py
@@ -10,4 +10,22 @@ their digits.
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 """
 
+from utils import *
+from math import factorial
+from itertools import combinations_with_replacement as combos
+
+factorials = [factorial(i) for i in xrange(10)]
+
+good = []
+
+for n in xrange(2, 7):
+    for combo in combos(range(10), n):
+        s = sum((factorials[i] for i in combo))
+        digits = to_digits(s)
+        if len(digits) != n: continue
+        if combo == tuple(sorted(digits)):
+            good.append(s)
+
+print sum(good)
+
 
