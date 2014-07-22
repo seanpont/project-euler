62fe4e0d6a165813e22433a0a1f53ae04656f38e
Sean Pont <seanpont@gmail.com>
2014-07-22 08:38:00 -0700

    50: sum of consecutive primes: good one. required some nice optimizations

--- 050.py
+++ 050.py
@@ -17,3 +17,51 @@ consecutive primes?
 """
 
 
+from utils import *
+
+
+primes = list(PrimeSieve(1e6))
+
+
+cheat_sheet = [prime for prime in [0] + primes[:1000]]
+for i in xrange(1, len(cheat_sheet)):
+    cheat_sheet[i] += cheat_sheet[i-1]
+
+bookmark = 0
+
+
+def find_consecutive_sum(n, min_count=0):
+    global bookmark
+    tail = 0
+    while cheat_sheet[bookmark] < n:
+        bookmark += 1
+    head = bookmark
+    n -= cheat_sheet[head]
+    while head - tail >= min_count:
+        # print n, tail, head, sum(primes[tail:head])
+        if n > 0:
+            n -= primes[head]
+            head += 1
+        elif n < 0:
+            n += primes[tail]
+            tail += 1
+        elif n == 0:
+            return head-tail
+    return 0
+
+
+best_prime, best_count = 0, 0
+for prime in primes:
+    count = find_consecutive_sum(prime, best_count)
+    if count > best_count:
+        # print prime, count
+        best_prime, best_count = prime, count
+
+print best_prime
+
+
+
+
+
+
+
---
+++ 051.py
@@ -0,0 +1,19 @@
+"""
+Project Euler Problem #51
+==========================
+
+By replacing the 1^st digit of *57, it turns out that six of the possible
+values: 157, 257, 457, 557, 757, and 857, are all prime.
+
+By replacing the 3^rd and 4^th digits of 56**3 with the same digit, this
+5-digit number is the first example having seven primes, yielding the
+family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993. Consequently
+56003, being the first member of this family, is the smallest prime with
+this property.
+
+Find the smallest prime which, by replacing part of the number (not
+necessarily adjacent digits) with the same digit, is part of an eight
+prime value family.
+"""
+
+
