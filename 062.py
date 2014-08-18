a441fc6ffde3e10c6c9bd0dd8355ef67bb01f695
Sean Pont <seanpont@gmail.com>
2014-08-18 15:28:22 -0700

    62: cubism: easy

--- 062.py
+++ 062.py
@@ -11,4 +11,16 @@ Find the smallest cube for which exactly five permutations of its digits
 are cube.
 """
 
+from collections import defaultdict
+from utils import to_digits
+cubes = defaultdict(list)
+
+for n in xrange(10000):
+    cube_list = cubes[tuple(sorted(to_digits(n ** 3)))]
+    cube_list.append(n)
+
+cubists = filter(lambda c: len(c) >= 5, cubes.values())
+print min(map(lambda c: min(c), cubists)) ** 3
+
+
 
