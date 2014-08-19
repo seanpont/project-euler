19e3ffa06b724b73eac4f604e9f94139fd450036
Sean Pont <seanpont@gmail.com>
2014-08-18 18:40:18 -0700

    63: wrote the code, but I dont understand why it works fully

--- 063.py
+++ 063.py
@@ -9,3 +9,29 @@ How many n-digit positive integers exist which are also an nth power?
 """
 
 
+def num_digits(n):
+    c = 0
+    while n:
+        n /= 10
+        c += 1
+    return c
+
+assert num_digits(4024) == 4
+
+
+def find_powers_of_length(length):
+    n, p = 1, 1
+    while num_digits(p) <= length:
+        if num_digits(p) == length:
+            yield n
+        n += 1
+        p = n**length
+
+
+def solve():
+    count = 0
+    for length in xrange(1, 50):
+        count += len(tuple(find_powers_of_length(length)))
+    print count
+
+solve()
